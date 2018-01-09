63c407091a720469672858ac7096e8ab2d1643b1
Jacob1411 <33232575+Jacob1411@users.noreply.github.com>
2018-01-09 14:38:15 -0500

    Game

--- Assets/Scripts/LoadSceneOnKeyDown.cs
+++ Assets/Scripts/LoadSceneOnKeyDown.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LoadSceneOnKeyDown : MonoBehaviour {
+public class LoadSceneOnKeyDown : MonoBehaviour 
+{    
+    public string SceneToLoad;      
+    public float PreKeyDelay = 1f;
+    public float PostKeyDelay = 1f;       
+    public KeyCode Key = KeyCode.Return;
+       
+    IEnumerator Start()
+    {      
+        yield return new WaitForSeconds(PreKeyDelay);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        while (!Input.GetKeyDown(Key))
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(PostKeyDelay);
+              
+        SceneManager.LoadScene(SceneToLoad);            
+    }
 }
